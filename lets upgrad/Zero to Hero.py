9c4eb3da9e44c0cb6d7591c2ba7b1892ab3b58ec
Maaz4747 <90049418+Maaz4747@users.noreply.github.com>
2021-09-03 19:02:58 +0530

    Day 1 Letsupgrad form

---
+++ lets upgrad/Zero to Hero.py
@@ -0,0 +1,44 @@
+#print("hello world")
+'''
+hello how are you 
+'''
+#print("what we think we became")
+#print('''whats app''')
+from abc import abstractproperty
+
+
+marks= 10.99
+name="Siva"
+age=22
+'''
+print("The marks of ",name, "are",marks,"and the age is " ,age )
+print("the marks of %s are %.2f and the age is %d "%(name,marks,age))
+print(f"The MARKS OF {name} is {marks} and the age is {age}")
+'''
+'''
+ESCAPE SEQUENCE STRINGS
+\a sound system bell
+\n newline
+\t horizontal tab
+\\ the \ character 
+\' single quatation mark 
+\" double quotation mark 
+'''
+#print('python\'s world' )
+
+#DATA TYPES IN PYTHON
+'''
+int
+float
+boolean
+string
+tuple
+list
+dictionary
+set
+
+'''
+a=' MAAZ SAQLAIN 4789'
+a=a.lower()
+print(a)
+
